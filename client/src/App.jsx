00ad254d06e977abb08ccc9e48833eeeaffa823f
mohammededris <mohammededris139@gmail.com>
2026-07-08 00:33:18 +0300

    feat: Implement NotFound component with styling and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Courses } from "./components/Courses/Courses";
 import { Landing } from "./components/Landing/Landing";
 import { CourseDetails } from "./components/Courses/CourseDetails";
+
+function NotFound() {
+  return (
+    <div className="not-found">
+      <h1>Page is not available</h1>
+      <p>The page you are looking for does not exist.</p>
+      <Link to="/">Return Home</Link>
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -10,6 +20,7 @@ function App() {
       <Route path="/" element={<Landing />} />
       <Route path="/courses" element={<Courses />} />
       <Route path="/courses/:_id" element={<CourseDetails />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
